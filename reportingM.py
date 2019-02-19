036f5ccf7b454a5285382de194220c8f0b6ce8e3
kodw0402 <daewoong.ko@gatech.edu>
2019-02-19 02:17:50 -0500

    Add loop function

--- reportingM.py
+++ reportingM.py
@@ -7,7 +7,7 @@ def get_top3_articles():
     db = psycopg2.connect(database=DB_NAME)
     c = db.cursor()
     c.execute("select articles.title, count(slug) from articles, log where"
-              " path like '%' || slug group by title order by"
+              " path like '%' || slug group by title order by "
               "count(slug) desc limit 3;")
     return c.fetchall()
 
@@ -39,17 +39,25 @@ if __name__ == "__main__":
     print("1. The most popular three articles of all time")
     print("2. The most popular article authors of all time")
     print("3. The days more than 1% of requests led to errors\n")
-    number = input("Please type a number: ")
-    if number == 1:
-        info = get_top3_articles()
-        for i in range(len(info)):
-            print("{au} -- {vi} views".format(au=info[i][0], vi=info[i][1]))
-    if number == 2:
-        info = get_top_authors()
-        for i in range(len(info)):
-            print("{au} -- {vi} views".format(au=info[i][0], vi=info[i][1]))
-    if number == 3:
-        info = get_err_log()
-        for i in range(len(info)):
-            print("{au} -- {vi}% errors".format(au=info[i][0], vi="{0:.2}"
-                                                .format(info[i][1])))
+    while True:
+        number = input("Please type a number (exit: 0): ")
+        if number == 0:
+            break
+        elif number == 1:
+            info = get_top3_articles()
+            for i in range(len(info)):
+                print("{au} -- {vi} views".format(au=info[i][0],
+                                                  vi=info[i][1]))
+            print("\n")
+        elif number == 2:
+            info = get_top_authors()
+            for i in range(len(info)):
+                print("{au} -- {vi} views".format(au=info[i][0],
+                                                  vi=info[i][1]))
+            print("\n")
+        elif number == 3:
+            info = get_err_log()
+            for i in range(len(info)):
+                print("{au} -- {vi}% errors".format(au=info[i][0], vi="{0:.2}"
+                                                    .format(info[i][1])))
+            print("\n")
